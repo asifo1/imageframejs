0614142847e8b61de365c9fb8c41e317604100ef
asifo1 <a.shahriar1999@gmail.com>
2020-12-10 23:00:40 +0600

    Done positioning

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const content = document.querySelector(".content")
 const frame = document.querySelector(".frame")
+var windowSize = document.documentElement.clientWidth
 var limit = 10
 const render = ()=>{
     let imgSize = (document.documentElement.clientWidth/100)*9
     let imgPositionTop = 0, imgPositionBottom = 0, imgPositionLeft = imgSize, imgPositionRight = imgSize
     let row = Math.ceil(content.clientHeight/imgSize)
     content.style.width = `${imgSize*(limit-2)}px`
-    // content.style.height = `${(row*imgSize).toFixed(3)}px`
-    // content.style.paddingBottom = `${row*imgSize.toFixed(3)}px`
-    console.log(row,content.clientHeight,(row*imgSize))
+    content.style.height = `${(row*imgSize).toFixed(2)}px`
 
     //Render Top images
     for(i=1;i<=limit;i++){
@@ -25,19 +24,19 @@ const render = ()=>{
     }
 
     
-        //Render right images
-        for(i=limit+1;i<=limit+row;i++){
-            img3 = document.createElement("img")
-            img3.src = `images/grid/${i}.jpg`
-            img3.style.width = `${imgSize}px`
-            img3.style.height = `${imgSize}px`
-            img3.style.position = "absolute"
-            img3.style.right = "0" 
-            img3.style.top = `${imgPositionRight}px`
-            img3.setAttribute("class","image")
-            imgPositionRight += imgSize
-            frame.appendChild(img3)
-        }
+    //Render right images
+    for(i=limit+1;i<=limit+row;i++){
+        img3 = document.createElement("img")
+        img3.src = `images/grid/${i}.jpg`
+        img3.style.width = `${imgSize}px`
+        img3.style.height = `${imgSize}px`
+        img3.style.position = "absolute"
+        img3.style.right = "0" 
+        img3.style.top = `${imgPositionRight}px`
+        img3.setAttribute("class","image")
+        imgPositionRight += imgSize
+        frame.appendChild(img3)
+    }
 
     //Render Bottom images
     for(i=2*limit+row;i>=limit+row+1;i--){
@@ -66,11 +65,6 @@ const render = ()=>{
         imgPositionLeft += imgSize
         frame.appendChild(img3)
     }
-
-
-
-
-
     frame.style.padding = `${imgSize}px`
 }
 
@@ -84,6 +78,7 @@ const removeImages = () =>{
 
 window.onresize = () => {
     removeImages()
+    content.style.height = "auto"
     if(document.documentElement.clientWidth<780){
         limit = 8
     }
